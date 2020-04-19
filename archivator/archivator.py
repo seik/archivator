17c7ff1cc32467454faeeb54e4c98e1e2da13b93
Iván José Martín García <ivanmartinga@gmail.com>
2020-04-20 01:24:31 +0200

    Initial CLI

--- archivator/archivator.py
+++ archivator/archivator.py
@@ -1,21 +1,32 @@
+import html
 from time import sleep
+from typing import Tuple
 from urllib.parse import urlparse
+import click
 
 import requests
 from bs4 import BeautifulSoup
 from bs4.element import SoupStrainer
 from loguru import logger
-import html
+
 from archivator.archiveorg import InternetArchive
 
 
-class Scraper:
+class Archivator:
     def __init__(self, start_url):
         parsed_url = urlparse(start_url)
         self.base_url = f"{parsed_url.scheme}://{parsed_url.hostname}"
         self.start_url = start_url
         self.scraped_urls = set()
         self.urls_to_scrape = set([start_url])
+
+    @staticmethod
+    def archive_url(url: str) -> Tuple[str, bool]:
+        """
+        Static method used to archive a single link
+        """
+        internet_archive = InternetArchive()
+        return internet_archive.archive_page(url)
 
     def check_is_site_url(self, url: str) -> bool:
         return bool(url.startswith("/") or self.base_url in url)
@@ -58,24 +69,19 @@ class Scraper:
         internet_archive = InternetArchive()
         for url in self.scraped_urls:
             internet_archive.archive_page(url)
-            logger.info(f"Archived: {url}")
 
     def run(self):
         while self.urls_to_scrape:
             current_url = self.urls_to_scrape.pop()
             urls = self.collect_page_urls(current_url)
             self.scraped_urls.add(current_url)
-            logger.info(f"Scrapping {current_url}")
+            logger.debug(f"Scrapping {current_url}")
             for url in urls:
                 if url not in self.scraped_urls and url not in self.urls_to_scrape:
                     self.urls_to_scrape.add(url)
-                    logger.info(f"Found {url}")
+                    logger.debug(f"Found {url}")
 
-        logger.info(f"Collected {len(self.scraped_urls)} URLs")
-        logger.info(f"Start archiving")
+        click.echo(f"Collected {len(self.scraped_urls)} URLs")
+        click.echo(f"Start archiving")
 
         self.archive_urls()
-
-
-scraper = Scraper("https://ivmg.me/")
-scraper.run()
--- archivator/archiveorg.py
+++ archivator/archiveorg.py
@@ -1,4 +1,6 @@
+from typing import Tuple
 from urllib.parse import urljoin
+
 import requests
 
 
@@ -8,6 +10,45 @@ class InternetArchive:
         self.user_agent = "archivator.py"
         self.save_url = urljoin(self.api_url, "/save/")
 
-    def archive_page(self, url: str):
+    class BlockedError(Exception):
+        """
+        Returned when the internet archive has been banned from the site
+        """
+
+    class ArchiveError(Exception):
+        """
+        General exception for general errors
+        """
+
+    def archive_page(self, url: str) -> Tuple[str, bool]:
         archive_url = f"{self.save_url}{url}"
-        requests.get(archive_url, headers={"User-Agent": self.user_agent})
+        response = requests.get(archive_url, headers={"User-Agent": self.user_agent})
+
+        has_error_header = "X-Archive-Wayback-Runtime-Error" in response.headers
+        if has_error_header:
+            error_header = response.headers["X-Archive-Wayback-Runtime-Error"]
+            if error_header == "RobotAccessControlException: Blocked By Robots":
+                raise BlockedError(
+                    "internet archive returned blocked by robots.txt error"
+                )
+            else:
+                raise ArchiveError(error_header)
+
+        if response.status_code in [403, 502]:
+            raise ArchiveError(response.headers)
+
+        try:
+            archive_id = response.headers["Content-Location"]
+        except KeyError:
+            # If it can't find that key raise the error
+            raise ArchiveError(
+                dict(status_code=response.status_code, headers=response.headers)
+            )
+
+        archive_url = urljoin(self.api_url, archive_id)
+
+        cached = (
+            "X-Page-Cache" in response.headers
+            and response.headers["X-Page-Cache"] == "HIT"
+        )
+        return archive_url, cached
